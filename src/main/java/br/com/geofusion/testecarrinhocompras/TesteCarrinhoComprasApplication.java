809f2d3f43c566312dcef54c58677a1d0b1cd414
LucasLottiPzm <lucaslotti300@gmail.com>
2022-10-25 23:08:53 -0300

    iniciando repositório

---
+++ src/main/java/br/com/geofusion/testecarrinhocompras/TesteCarrinhoComprasApplication.java
@@ -0,0 +1,13 @@
+package br.com.geofusion.testecarrinhocompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCarrinhoComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteCarrinhoComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/geofusion/testecarrinhocompras/TesteCarrinhoComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geofusion.testecarrinhocompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteCarrinhoComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
